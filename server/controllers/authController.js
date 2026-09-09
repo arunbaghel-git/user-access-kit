7e86cd924f290eee72c92636f3a808abeea32c8c
arun <arun@progress.com>
2026-09-10 04:10:59 +0530

    add login validation and JWT authentication — verify credentials and keep users logged in

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,7 +18,7 @@ export const registerUser = async (req, res) => {
         message: "user already exist",
       });
     }
-    const hashedPassword = await bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new userModel({ name, email, password: hashedPassword });
     await newUser.save();
     // create token
@@ -33,11 +33,63 @@ export const registerUser = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.json({
-        success:true,
-        message:"user register successfully"
-    })
+      success: true,
+      message: "user register successfully",
+      newUser, // for test in postman only return full newUser object
+    });
   } catch (err) {
-    res.json({
+    return res.json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "missing fields details",
+    });
+  }
+  try {
+    const existingUser = await userModel.findOne({ email });
+    if (!existingUser) {
+      return res.json({
+        success: false,
+        message: "email is invalid",
+      });
+    }
+    const isPasswordMatch = await bcrypt.compare(
+      password,
+      existingUser.password,
+    );
+    if (!isPasswordMatch) {
+      return res.json({
+        success: false,
+        message: "invalid credentials",
+      });
+    }
+    const newToken = jwt.sign(
+      { id: existingUser._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+    res.cookie("token", newToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.json({
+      success: true,
+      message: "user login successfully",
+    });
+  } catch (err) {
+    return res.json({
       success: false,
       message: err.message,
     });
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { registerUser } from "../controllers/authController";
+import { registerUser,loginUser } from "../controllers/authController.js";
 
 export const route = express.Router();
 
 route.post('/register',registerUser);
+route.get('/login',loginUser);
